a5b8f8c87e98cb04aa3e81810566a492ec3f9ede
Matt Istvan <istvan@pdx.edu>
2023-05-21 06:24:17 -0400

    First pass implementation for a 'location history' entity that will track a users location information in-game

---
+++ backend/src/db/entities/LocationHistory.ts
@@ -0,0 +1,48 @@
+import { Entity, Property, Unique, Collection } from "@mikro-orm/core";
+import { User } from "./User.js"
+
+/*
+Locations information for an individual user, can be values 0, 1, 2
+0: Undiscovered and unvisited, will not show on the main map
+1: Discovered but unvisited, will show on main map but interaction buttons will not
+	 appear until the user selects 'look around' in the location
+2: Discovered and visited, will show on main map and show interaction buttons when
+   visited.
+
+user: this list will be tied to a user, it will reflect their location history
+	    unique property because every user will have only 1 location history
+ */
+
+
+@Entity()
+export class LocationHistory {
+	//
+	@Property()
+	@Unique()
+	user!: User;
+	
+	//farm location will be discovered by default because it is the starting area
+	@Property()
+	farm: 1;
+	
+	//castlegate will be undiscovered/unvisited initially but will be discoverable
+	//by looking around at the farm
+	@Property()
+	castleGate: 0;
+	
+	//outsideCastleWalls will be undiscovered/unvisited initially but will be discoverable
+	//by looking around at the castlegate
+	@Property()
+	outsideCastleWalls: 0;
+	
+	//backstreets will be undiscovered/unvisited initially but will be discoverable
+	//by using the shovel at the outsideCastleWalls location
+	@Property()
+	backstreets: 0;
+	
+	//towncenter will be undiscovered/unvisited initially but will be discoverable by
+	//looking around in the backstreets
+	@Property()
+	townCenter: 0;
+	
+}
