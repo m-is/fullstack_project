467f55e9dcbae739553e2b159e73014fe0177853
Matt Istvan <istvan@pdx.edu>
2023-05-21 06:55:50 -0400

    First pass implementation of a user inventory

---
+++ backend/src/db/entities/Inventory.ts
@@ -0,0 +1,25 @@
+import { Entity, Property, Unique, Collection } from "@mikro-orm/core";
+import { User } from "./User.js"
+
+/*
+Inventory for a specific user, tracks their inventory in-game
+Items are bools, true for acquired, false for not acquired/used/broken/etc.
+																		        (items can be lost in different ways)
+ */
+
+
+@Entity()
+export class Inventory {
+	//Inventory is tied to one specific user, a user may not have two inventories
+	@Unique()
+	@Property()
+	user!: User;
+	
+	//Shovel item, found at the farm when inspecting the shed
+	@Property()
+	shovel: false;
+	
+	//Lucky coin item, found at the city center when inspecting the fountain
+	@Property()
+	luckyCoin: false;
+}
