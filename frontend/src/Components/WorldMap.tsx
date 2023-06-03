e5d9fb190ad16be1462f5fd5cd74eb63cb05f0d3
Matt Istvan <istvan@pdx.edu>
2023-06-02 22:25:09 -0400

    Dynamic world map implementation first-pass

--- frontend/src/Components/WorldMap.tsx
+++ frontend/src/Components/WorldMap.tsx
@@ -3,12 +3,57 @@
 //If player tries to visit undiscovered location give them a "you are lost" page
 
 
+import { useAuth } from "@/Services/Auth.tsx";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export const WorldMap = () => {
+	const auth = useAuth();
+	
+	const[locations,setLocations] = useState();
+	const[map, setMap] = useState([]);
+	
+	
+	useEffect( () => {
+		const getLocations = async () => {
+			const locationsRes = await axios({
+				method: 'search',
+				url: 'http://localhost:8080/location',
+				data:{ userId: auth.userId }
+				}
+			);
+		
+			return locationsRes.data;
+		};
+		
+		getLocations().then(setLocations);
+	},[auth.userId]);
+	
+	useEffect( () => {
+		if(locations!=undefined){
+			// @ts-ignore
+			locations.forEach((location) =>{
+				const values = Object.values(location);
+				map.push(values[1]);
+			});
+		}
+		setMap(map);
+	},[locations]);
+	
+	
+	
+	
 	return (
 		<div>
-			<FarmIcon />
-			<CastleGateIcon />
-			<TownCenterIcon />
+			{ map.includes("Farm") ? (
+				<FarmIcon />
+			) : null }
+			{ map.includes("castleGate") ? (
+				<CastleGateIcon />
+			) : null }
+			{ map.includes("townCenter") ? (
+				<TownCenterIcon />
+			) : null }
 		</div>
 	);
 };
